635de946f5862f9df766596c7722511fa8ee0b3b
kobai <vkobayashi106@gmail.com>
2020-01-05 15:25:15 -0500

    remove console log

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
 import firebase from 'firebase'
 
-console.log(process.env)
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
