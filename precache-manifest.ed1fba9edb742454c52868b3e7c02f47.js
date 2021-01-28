b8c70d1026ccdd4a6b631a978561efc5eaae05d5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-28 14:57:26 +0000

    Deploy Kobai/personal-website to github.com/Kobai/personal-website.git:gh-pages

---
+++ precache-manifest.ed1fba9edb742454c52868b3e7c02f47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3c1515201171e3c1333b375bdbfb55",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "bd892f41c65f344112b7",
+    "url": "/personal-website/static/css/main.21ae36c4.chunk.css"
+  },
+  {
+    "revision": "52b1debb9466e1a23dc0",
+    "url": "/personal-website/static/js/2.11159862.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/personal-website/static/js/2.11159862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd892f41c65f344112b7",
+    "url": "/personal-website/static/js/main.6dc53a54.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/personal-website/static/media/octocat.3e54ed15.png"
+  }
+]);
